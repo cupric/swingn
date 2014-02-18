48f694bec5de0f01de20898b93c9a87b41af23c4
Shamtasm <git+shamtasm@doornbos.net>
2014-02-17 21:14:54 -0800

    Log for swingn.

---
+++ src/main/java/swingn/Log.java
@@ -0,0 +1,13 @@
+//
+// SwingN - swing-like utilities and widgets for use with tripleplay
+// Copyright (c) 2014, Cupric - All rights reserved.
+// http://github.com/cupric/swingn/blob/master/LICENSE
+
+package swingn;
+
+import tripleplay.util.Logger;
+
+public class Log
+{
+    public static final Logger log = new Logger("swingn");
+}
